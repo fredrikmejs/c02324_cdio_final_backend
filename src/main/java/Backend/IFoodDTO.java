e18255b3e8e9736a9126f318dfbc805888b09189
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-07 09:49:52 +0200

    Added IFoodDTO.java

---
+++ src/main/java/Backend/IFoodDTO.java
@@ -0,0 +1,26 @@
+package Backend;
+
+import java.sql.Date;
+
+public interface IFoodDTO {
+    //Expiration Dates
+    void setExpDate(Date date);
+    Date getExpDate();
+    //TODO: Amount - redacted for now
+
+    //TODO: Unit - redacted for now
+
+    //Name
+    void setName(String name);
+    String getName();
+
+    //Location
+    //TODO: Create enum for location (Freezer, Fridge, Pantry)
+    void setLocation(ELocation location);
+    ELocation getLocation();
+
+    //Category
+    //TODO: Create enum for category (Fish, meats, vegetables etc.)
+    void setCategory(ECategory category)
+    ECategory getCategory();
+}
