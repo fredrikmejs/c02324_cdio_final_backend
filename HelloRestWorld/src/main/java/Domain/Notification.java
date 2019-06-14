9d230af106d6679c078200135df80505d7050c4b
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-14 17:00:34 +0200

    Added TODO to Notification.java

--- HelloRestWorld/src/main/java/Domain/Notification.java
+++ HelloRestWorld/src/main/java/Domain/Notification.java
@@ -4,7 +4,7 @@ import Technical_Services.IFoodDTO;
 
 import java.sql.Date;
 import java.util.List;
-
+//TODO: FIX THIS AND IMPLEMENT IN ERRORHANDLING INSTEAD
 public class Notification implements INotification {
     @Override
     public List<IFoodDTO> getExpFood(String userName) {
