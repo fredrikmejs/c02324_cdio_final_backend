54900773b226c4559c7f9a275d85dd593b924cd2
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-18 11:57:46 +0200

    Added documentation to AppConfig.java

--- HelloRestWorld/src/main/java/rest/AppConfig.java
+++ HelloRestWorld/src/main/java/rest/AppConfig.java
@@ -4,6 +4,9 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
 
+/**
+ * Class is the foundation for the REST implementation
+ */
 @ApplicationPath("/rest")
 public class AppConfig extends ResourceConfig {
 }
