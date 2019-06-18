38cc9733f38ca6b3578a76b3f9d1e08ab346f71e
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-18 11:09:02 +0200

    Added documentation to HelloService.java

--- HelloRestWorld/src/main/java/rest/HelloService.java
+++ HelloRestWorld/src/main/java/rest/HelloService.java
@@ -2,6 +2,7 @@ package rest;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import Technical_Services.ELocation;
 import com.google.gson.JsonObject;
@@ -12,20 +13,24 @@ import static rest.DTS.dateToString;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloService {
-
+    /**
+     * Method is to test REST functionality on a most basic level.
+     * @param id ID is the ID to be printed.
+     * @return returns HTTP status code 200 and a JSON object
+     */
     @GET
     @Path("{id}")
-    public String helloWorld(@PathParam("id")int id){
+    public Response helloWorld(@PathParam("id")int id){
+//        Prints Hello World!.. to the console
         System.out.println("Hello world! This was a GET request!"); //Tests
+//        Creates a JsonObject and adds properties to it
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("msg", "Hello World!");
         jsonObject.addProperty("id", id);
+//        Prints the JsonObject to the console
         System.out.println("to:\t" + jsonObject.toString()); //Tests
-        System.out.println(ELocation.Fridge.toString());
-        String date = dateToString(20, 06, 2019);
-
-        System.out.println(date);
-        return jsonObject.toString();
+//        Returns HTTP response status 200 and a JsonObject
+        return Response.status(200).entity(jsonObject.toString()).build();
     }
 
 }
