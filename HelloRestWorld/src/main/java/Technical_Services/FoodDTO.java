27faef2d79a8305902d610b3ff010458458f3269
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-06-20 10:02:02 +0200

    Changed dates to always have 3 hours added to them

--- HelloRestWorld/src/main/java/Technical_Services/FoodDTO.java
+++ HelloRestWorld/src/main/java/Technical_Services/FoodDTO.java
@@ -26,7 +26,7 @@ public class FoodDTO extends Equals implements IFoodDTO {
      */
     public FoodDTO(String foodName, long date, ELocation location, ECategory category, String userName){
 //      Variables are initialised
-    this.expDate = date + (1000*60*60*12);//The supplied date is added 12 additional hours to compensate for DST and MySQL incompatibility
+    this.expDate = date + (1000*60*60*3);//The supplied date is added 12 additional hours to compensate for DST and MySQL incompatibility
     this.name = foodName;
     this.category = category;
     this.location = location;
@@ -45,7 +45,7 @@ public class FoodDTO extends Equals implements IFoodDTO {
      */
     public FoodDTO(int food_id, String foodName, long date, ELocation location, ECategory category, String userName){
 //        Variables are now initialised
-        this.expDate = date + (1000*60*60*12);//12 hours are added to fix compatibility issues
+        this.expDate = date + (1000*60*60*3);//12 hours are added to fix compatibility issues
         this.name = foodName;
         this.category = category;
         this.location = location;
@@ -71,7 +71,7 @@ public class FoodDTO extends Equals implements IFoodDTO {
      * @param timeSinceEpochInMillis
      */
     public void setExpDate(long timeSinceEpochInMillis) {
-    this.expDate = timeSinceEpochInMillis + (1000*60*60*12); //Add 12 hours to day to hopefully fix off by 1 day because of DST
+    this.expDate = timeSinceEpochInMillis + (1000*60*60*3); //Add 12 hours to day to hopefully fix off by 1 day because of DST
     }
 
     /**
